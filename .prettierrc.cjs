53cae89bc18d3abd31282d58be5c1b316c0ce7ab
Rein Appeldoorn <reinzor@gmail.com>
2023-09-06 21:40:55 +0200

    Prettier set-up

---
+++ .prettierrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  plugins: ['prettier-plugin-svelte'],
+};
--- scripts/build-zip.cjs
+++ scripts/build-zip.cjs
@@ -5,35 +5,35 @@ const path = require('path');
 const archiver = require('archiver');
 
 const DIST_DIR = path.join(__dirname, '../dist');
-const DIST_ZIP_DIR = path.join(__dirname, '../dist-zip'); 
-const CHANGELOG = path.join(__dirname, '../CHANGELOG.rst'); 
-const DIST_CHANGELOG = path.join(__dirname, '../dist/CHANGELOG.rst'); 
+const DIST_ZIP_DIR = path.join(__dirname, '../dist-zip');
+const CHANGELOG = path.join(__dirname, '../CHANGELOG.rst');
+const DIST_CHANGELOG = path.join(__dirname, '../dist/CHANGELOG.rst');
 
 const extractExtensionData = () => {
   const extPackageJson = require('../package.json');
 
   return {
     name: extPackageJson.name,
-    version: extPackageJson.version
-  }
+    version: extPackageJson.version,
+  };
 };
 
 const makeDestZipDirIfNotExists = () => {
-  if(!fs.existsSync(DIST_ZIP_DIR)) {
+  if (!fs.existsSync(DIST_ZIP_DIR)) {
     fs.mkdirSync(DIST_ZIP_DIR);
   }
-}
+};
 
 const buildZip = (src, dist, zipFilename) => {
   console.info(`Building ${zipFilename}...`);
 
-  const archive = archiver('zip', { zlib: { level: 9 }});
+  const archive = archiver('zip', { zlib: { level: 9 } });
   const stream = fs.createWriteStream(path.join(dist, zipFilename));
-  
+
   return new Promise((resolve, reject) => {
     archive
       .directory(src, false)
-      .on('error', err => reject(err))
+      .on('error', (err) => reject(err))
       .pipe(stream);
 
     stream.on('close', () => resolve());
@@ -42,16 +42,16 @@ const buildZip = (src, dist, zipFilename) => {
 };
 
 const main = () => {
-  const {name, version} = extractExtensionData();
+  const { name, version } = extractExtensionData();
   const zipFilename = `${name}-v${version}.zip`;
-  
+
   makeDestZipDirIfNotExists();
 
-  fs.copyFile(CHANGELOG, DIST_CHANGELOG, (err) => {})
+  fs.copyFile(CHANGELOG, DIST_CHANGELOG, (err) => {});
 
   buildZip(DIST_DIR, DIST_ZIP_DIR, zipFilename)
     .then(() => console.info('OK'))
-    .catch(console.err); 
+    .catch(console.err);
 };
 
 main();
